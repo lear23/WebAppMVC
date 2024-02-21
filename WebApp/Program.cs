fb08c5e06f1f6f7da9b97a01ac3c39dece93db1a
lear23 <130910243+lear23@users.noreply.github.com>
2024-02-21 13:43:17 +0100

    fixad link

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -4,16 +4,12 @@ builder.Services.AddControllersWithViews();
 
 
 
-
 var app = builder.Build();
 app.UseHsts();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
